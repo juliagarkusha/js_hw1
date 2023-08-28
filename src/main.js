a38445f1c754c4e2c7630db85ec2dba9f6bce734
magna <julijagarkusha@gmail.com>
2023-08-29 01:39:53 +0300

    added domashka

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log('Hey, this is my first JS program: ');
+const age = 24;
+let message = `Вам ${age} `;
+
+if (age % 10 === 1 && age % 100 !== 11) {
+    message += "рік";
+} else if (age % 10 >= 2 && age % 10 <= 4 && (age % 100 < 10 || age % 100 >= 20)) {
+    message += "роки";
+} else {
+    message += "років";
+}
+
+console.log(message);

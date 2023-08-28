c80188532399138a2d7c6e410531b1ea42267620
magna <julijagarkusha@gmail.com>
2023-08-29 00:58:57 +0300

    added domashka 3

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log('Hey, this is my first JS program: ');
+const number = 15;
+let message = '';
+
+if (number % 3 === 0) {
+    message += 'Fizz';
+}
+
+if (number % 5 === 0) {
+    message += 'Buzz';
+}
+
+console.log(message);

64ccffafd2788cc3e1558c8d9d4a14b699a73e42
magna <julijagarkusha@gmail.com>
2023-08-29 01:26:00 +0300

    added domashka

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log('Hey, this is my first JS program: ');
+const year = 2024;
+
+if((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    console.log(`${year} рік - високосний`);
+} else {
+    console.log(`${year} рік - не високосний`);
+}
